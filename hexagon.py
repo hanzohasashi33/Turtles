4791ba67d4792f8587029cab99c77703f3931d34
Hanzo hasashi <54119123+hanzohasashi33@users.noreply.github.com>
2019-11-05 19:18:10 +0530

    Add files via upload
    Create a hexagon using turtle in python

---
+++ hexagon.py
@@ -0,0 +1,11 @@
+import turtle
+
+hexagon = turtle.Turtle()        #assigning a turtle pointer named hexagon
+
+for i in range(6):
+    hexagon.forward(100) 
+    hexagon.right(60)           #similar to star
+
+
+turtle.done()
+
