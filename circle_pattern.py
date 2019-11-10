e2b98e51bf2c69e438badab2162658402caafe38
Prasanna Venkatesh <54119123+hanzohasashi33@users.noreply.github.com>
2019-11-10 18:35:58 +0530

    Update circle_pattern.py

--- circle_pattern.py
+++ circle_pattern.py
@@ -10,5 +10,14 @@ for i in range(30):
     circle_pattern.circle(5 * i)
     circle_pattern.circle(-5 * i)
     circle_pattern.left(i)
+    
+'''
+If radius is positive, then the arc starts radius turtle steps behind the turtle. 
+If radius is negative, then the arc starts radius turtle steps in front of the turtle. 
+In both cases, the arc sweeps an angle equal to the angle input.
+If angle is positive, then the arc is drawn in a clockwise direction. 
+If angle is negative, then the arc is drawn in a counter-clockwise direction. 
+If angle is 360 or greater (or -360 or less), then ARC draws a circle.
+'''
 
 turtle.exitonclick()
